73e3c2313712248edb26669d8bf81ecce6096f96
Your Name <you@example.com>
2023-01-30 13:33:56 +0700

    update

---
+++ assets/js/menu.js
@@ -0,0 +1,10 @@
+// const contextmenu = document.getElementById("menu");
+// document.addEventListener('contextmenu', function(event) {
+// 	event.preventDefault();
+// 	contextmenu.style.top = event.clientY + 'px';
+// 	contextmenu.style.left = event.clientX + 'px';
+// 	contextmenu.style.display = 'block';
+// })
+// document.addEventListener('click', function() {
+// 	contextmenu.style.display = 'none';
+// })
